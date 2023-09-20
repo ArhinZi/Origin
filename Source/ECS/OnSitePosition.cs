ad532304246c87ed43462cf4b8a7283d24d2a0c8
Timofey <Timofey45.16.95@gmail.com>
2023-09-20 19:24:50 +0300

    some changes

--- Source/ECS/OnSitePosition.cs
+++ Source/ECS/OnSitePosition.cs
@@ -7,14 +7,12 @@ namespace Origin.Source.ECS
 {
     public struct OnSitePosition
     {
-        public Site site;
         public Point3 position;
 
         public IsometricDirection DirectionOfView;
 
-        public OnSitePosition(Site site, Point3 pos, IsometricDirection dir = IsometricDirection.NONE)
+        public OnSitePosition(Point3 pos, IsometricDirection dir = IsometricDirection.NONE)
         {
-            this.site = site;
             this.position = pos;
             DirectionOfView = dir;
         }
